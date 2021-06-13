b88726c14baa4340570d8cc22e4f099ec31a14b6
Nadiia <npodufala15@gmail.com>
2021-06-13 11:38:15 +0300

    add search-pannel

---
+++ src/components/search-pannel/search-pannel.js
@@ -0,0 +1,25 @@
+import React, {Component} from "react";
+import './search-pannel.css';
+
+export default class SearchPannel extends Component {
+
+state = {
+    term: ''
+};
+
+onSearchChange = (e) => {
+    const term = e.target.value;
+    this.setState({term});
+    this.props.onSearchChange(term);
+};
+
+    render() {
+        return (  
+            <input type="text"
+            className = "form-control search-input"
+            placeholder="suchen"
+            value={this.state.term}
+            onChange={this.onSearchChange}/>
+            );
+    }
+}
