a8d49908d937ab0ad7f1af196d5dec671e5d44b2
Nadiia <npodufala15@gmail.com>
2021-06-13 11:36:50 +0300

    add item-status-filter

---
+++ src/components/item-status-filter/item-status-filter.js
@@ -0,0 +1,36 @@
+import React, {Component} from "react"
+import './item-status-filter.css';
+
+export default class ItemStatusFilter extends Component {
+    
+    buttons = [
+        {name: 'all',label: 'Alle'},
+        {name: 'active',label: 'Aktiv'},
+        {name: 'done',label: 'Getan'}
+
+    ];
+    
+    render() {
+const {filter, onFilterChange} = this.props;
+
+         const buttons = this.buttons.map(({name, label}) => {
+             const isActive = filter === name;
+             const clazz = isActive ? 'btn-info' : 'btn-outline-secondary'; 
+             return (
+<button type="button"
+                className={`btn ${clazz}`}
+                key={name}
+                onClick={() => onFilterChange(name)}>
+
+                    {label}
+                    </button>
+             );
+         });
+        return (
+            <div className="btn-group">
+               {buttons} 
+                </div>
+        );
+    }
+}
+
